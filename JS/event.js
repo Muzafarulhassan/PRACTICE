1eedf6720b3870298080695bbbbd9a74c2a41f8d
muzafarulhassan <muzafarulhassan86@gmail.com>
2026-01-09 10:42:21 -0800

    js

---
+++ JS/event.js
@@ -0,0 +1,30 @@
+var a=90;
+var b=8;// declaration and initialization   var b=declaration + initialization=b=8
+        console.log("the value of a ",a)
+        console.log("the value of b ",b)
+var sum =a+b// 90+8=98
+        console.log("the value of sum ",sum)
+var sub =a-b
+        console.log("the value of sub ",sub)
+        var mul=a*b
+        console.log("the value of mul ",mul)
+var div=a/b
+        console.log("the value of div ",div)
+var mod=a%b
+        console.log("the value of mod ",mod)
+    // data types 3
+    // string ""
+    // number 35656346
+    // boolean true/false
+
+    var Name="john doe" //string
+    console.log("the value of name ",Name)
+    var age=43//number
+    console.log("the value of age ",age)
+    var isMarried=true//boolean
+    console.log("the value of isMarried ",isMarried)
+    
+    // variables and data types 
+    //variables types =2
+    //initilization and declaration
+    
