9e3f1a1623246d85a0a49a61e6c96f729ae516ce
muzafarulhassan <muzafarulhassan86@gmail.com>
2026-01-10 11:00:41 -0800

    js

---
+++ JS/Opr Var/program.js
@@ -0,0 +1,37 @@
+var a=20
+var b=10
+var sum=a+b
+console.log("the value of sum is ",sum)
+var sub=a-b
+console.log("the value of sub is ",sub)
+var mul=a*b
+console.log("the value of mul is ",mul) 
+var div=a/b
+console.log("the value of div is",div)
+var mod=a%b
+console.log("the value of mod is",mod )
+var Name="John"
+console.log("the value of Name is ",Name)
+var age=30
+console.log("the value of age is ",age)
+var ismarried=true
+console.log("the value of ismarried is ",ismarried)
+var firstNumber= 10 ;
+var secondNumber=20;
+console.log("the value of firstNumber==secondNumber is",firstNumber==secondNumber)
+console.log("the value of firstNumber===secondNumber is",firstNumber===secondNumber)
+console.log("the value of firstNumber<secondNumber is",firstNumber<secondNumber)
+console.log("the value of firstNumber>secondNumber is",firstNumber>secondNumber)
+console.log("the value of firstNumber!=secondNumber is",firstNumber!=secondNumber)
+console.log("the value of firstNumber!==secondNumber is",firstNumber!=secondNumber)
+
+
+
+
+
+
+
+
+
+
+
--- JS/event.js
+++ JS/event.js
@@ -27,4 +27,70 @@ var mod=a%b
     // variables and data types 
     //variables types =2
     //initilization and declaration
+    //types of operators =3
+    //arithmetic operators + - * / %
+    //assignment operators = += -= *= /= %=
+    //comparison operators == === != !== > < >= <=
+
     
+
+var a=10;
+var b=10;
+var c=10;
+var x=a+b+c
+ a=20;
+console.log("the value of x is  ",a)
+//opetraters
+///arithmetic operators
+
+var firstNumber=10;
+var secondNumber=10;
+var thirdNumber=10;
+ var sum=firstNumber+secondNumber+thirdNumber
+ console.log("the value of sum is ",sum)
+ var sub=firstNumber-secondNumber
+ console.log("the value of sub is ",sub)
+ var mul=firstNumber*secondNumber                               
+        console.log("the value of mul is ",mul)
+       //prefix increment ,++x,--x//pre means phala
+       //postfix increment x++,x--//post means baad me
+       //++a,--a,a++,a--
+       var a=1
+       var b=(++a)+(--a)+(++a)+(--a)+(++a)
+        console.log("the value of b is ",b)
+               var B=(a++)+(a--)+(++a)+(--a)+(--a)
+        console.log("the value of B is ",B)
+        //comaprison operators
+        
+var firstNumber=10;
+var secondNumber="100";
+
+
+console.log("the value of firstNumber==secondNumber is ",firstNumber==secondNumber)
+console.log("the value of firstNumber===secondNumber is ",firstNumber===secondNumber)
+console.log("the value of firstNumber<secondNumber is ",firstNumber<secondNumber)
+console.log("the value of firstNumber>secondNumber is ",firstNumber>secondNumber)
+console.log("the value of firstNumber<=secondNumber is ",firstNumber<secondNumber)
+console.log("the value of firstNumber>=secondNumber is ",firstNumber>=secondNumber)
+console.log("the value of firstNumber!=secondNumber is ",firstNumber!=secondNumber)
+console.log("the value of firstNumber!==secondNumber is ",firstNumber!==secondNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
