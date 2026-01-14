097be80c3facedcd975cae37821f485f6449d067
muzafarulhassan <muzafarulhassan86@gmail.com>
2026-01-14 10:37:17 -0800

    js

---
+++ JS/if/event.js
@@ -0,0 +1,12 @@
+var week = parseInt(prompt("enter a week"));
+if(week==1){console.log(" is monday");}
+else{console.log("not a monday");}
+
+if(week==1){console.log(" is monday");}
+else if(week==2){console.log(" is tuesday");}
+else if(week==3){console.log(" is wednesday");}
+else if(week==4){console.log(" is thursday");}
+else if(week==5){console.log(" is friday");}
+else if(week==6){console.log(" is saturday");}
+else if(week==7){console.log(" is sunday");}
+else{console.log("not a valid week");}  
---
+++ JS/loopswitch/event.js
@@ -0,0 +1,22 @@
+let result=parseInt(prompt("the value of weeks"))
+switch(result){
+case 1:
+console.log("Monday")
+break;
+case 2:
+console.log("Tuesday")
+break; 
+case 3:
+console.log("Wednesday")
+break;
+case 4:
+console.log("Thirsday")
+break;
+case 5:
+console.log("Friday")
+break;
+default:
+        console.log("invalid week number ")
+}
+
+
