145b146deab154de43a43aad7ef45b7a807bea37
muzafarulhassan <muzafarulhassan86@gmail.com>
2026-03-08 00:53:18 -0800

    js

--- JS/assignment 1/event.js
+++ JS/assignment 1/event.js
@@ -74,4 +74,103 @@
 // var area= breadth*length
 // console.log("area of a rectangle is",area)
 
+//10. Write a JS program to calculate the square of a given number.
+
+// var num=10
+// var square=num*num
+// console.log("square of a given number is",square)
+
+// 11.Write a JS program to find the average of three numbers.
+
+// var a=10
+// var b=20
+// var c=30
+// var avarag=(a+b+c)/3
+// console.log("avarage of three numbers is",avarag)
+
+
+// 12.Write a JS program to swap the values of two variables using a third variable.
+
+// var a="Ali"
+// var b="Ahmed"
+// var temp
+// temp=a
+// a=b
+// b=temp
+// console.log("value of a is",a)
+// console.log("vlue of b is",b)
+
+// 13.Write a JS program to check whether a number is greater than 100 or not.
+
+// var num=150
+// if(num>100){
+//     console.log("The number is gtater than 100",num)
+// }else{
+//     console.log("The number is less than 100",num)
+// }
+
+
+// 14.Write a JS program to compare two numbers and display which one is greater.
+
+// var a=40
+// var b=30
+// if(a>b){
+//     console.log("a is grater than b",a)
+// }else if(a<b){
+//     console.log("b is grater than a",b)
+// }else{
+//     console.log("a and b are equal",a,b)
+// }
+
+// 15.Write a JS program to calculate the total and average marks of 5 subjects.
+
+// var sub1=80
+// var sub2=90
+// var sub3=70
+// var sub4=85
+// var sub5=95
+// var total=sub1+sub2+sub3+sub4+sub5
+// var average=total/5
+// console.log("total marks is",total)
+// console.log("average marks is",average)
+
+
+// 16.Write a JS program to demonstrate the use of all arithmetic operators: +, -, *, /, %, 
+
+// var a=10
+// var b=5
+// var sum=a+b
+// console.log("sum is",sum)
+// var sub=a-b
+// console.log("sub is",sub)
+// var mul=a*b
+// console.log("mul is",mul)
+// var dev=a/b
+// console.log("dev is",dev)
+// var mod=a%b
+// console.log("mod is",mod)
+
+
+// 17.Write a JS program to check whether the entered age is greater than or equal to 18 (eligible to vote or not).
+
+// var age=18
+// if(age>=18){
+//     console.log("you are eligible to vote",age)
+// }else{
+//     console.log("You are not eligible to vote",age)
+// }
+
+
+
+// 18.Write a JS program to compare two strings and check if they are equal or not.
+
+// var str1="Hello"
+// var str2="Hello"
+// if(str1===str2){
+//     console.log("The strings are equal",str1,str2)
+// }else{
+//     console.log("The strings are not equal",str1,str2)
+// }
+
+
 
