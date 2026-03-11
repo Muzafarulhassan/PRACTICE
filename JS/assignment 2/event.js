6dc5acaae249664cbe0cee2a4c3637d8528bb43e
muzafarulhassan <muzafarulhassan86@gmail.com>
2026-03-11 11:43:29 -0700

    js

---
+++ JS/assignment 2/event.js
@@ -0,0 +1,71 @@
+// var name=prompt("enter your name");
+// var age=+prompt("enter your age");
+// console.log("enter u name or age",name,age)
+
+// var num=[1,2,3,4,5,6,7,8,9,10]
+// for(var i=0;i<num.length;i++){
+//     console.log("number is",num[i])
+// }
+
+// var num=prompt("enter a number")
+// for(var i=1;i<=10;i++){
+//     console.log(num,"*",i,"=",num*i)
+// }
+
+// var num=1
+// while(num<=50){
+//     console.log("num",num)
+//     num++
+// }
+
+// var num=0
+// for(var i=1;i<=50;i++){
+//     num=num+i
+// }console.log("sum of 1 to 50 is",+num)
+
+// var num=0
+// var sum;
+// do{
+//     sum=prompt("Enter u number")
+//     num=num+sum
+//     num++
+// }while(sum==0)
+//      console.log("sum of number is",num)  
+
+// var i=15
+// while(i>=1){
+//     console.log("num",i)
+//     i--
+// }
+
+// var correctpassword="34"
+// var password;
+// do{password=prompt("enter your password")
+//     password=+password
+// }while(password===correctpassword)
+// console.log("correct password")
+
+
+
+// var correctPassword = "12345";
+// var password;
+
+// do {
+//   password = prompt("Enter your password:");
+
+//   if (password !== correctPassword) {
+//     console.log("Wrong password, try again!");
+//   }
+
+// } while (password !== correctPassword);
+
+// console.log("Access Granted!");
+
+
+
+// var num=prompt("enter a number")
+// var factrial=1
+// for(var i=1;i<=num;i++){
+//     factrial=factrial*i   
+// }
+// console.log("factorial of",num,"is",factrial)
