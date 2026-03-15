b12a1a6c18273bb3d11e8dfd0d2d7606ecedf214
muzafarulhassan <muzafarulhassan86@gmail.com>
2026-03-15 10:26:51 -0700

    js

---
+++ JS/assignment 3/evemt.js
@@ -0,0 +1,39 @@
+// Print all fruits from the array one by one.
+
+// var fruits = ["apple", "mango", "banana", "orange"];
+// console.log(fruits);
+
+
+// 2. Ask the user for any fruit name and check if it exists in the fruits array.
+
+// var userFruit = prompt("Enter a fruit name:");
+// var fruits = ["apple", "mango", "banana", "orange"];
+// if (fruits.includes(userFruit)) {
+//     console.log("Fruit is available:", userFruit);
+// } else {
+//     console.log("Fruit is not available:", userFruit);
+// }
+
+// // 3. Convert all fruits array values into uppercase using a loop.
+
+// var fruits = ["apple", "mango", "banana", "orange"];
+// for (var i = 0; i < fruits.length; i++) {
+//     fruits[i] = fruits[i].toUpperCase();
+// }
+// console.log(fruits);
+
+
+
+// // Convert all fruits array values into lowercase using a loop.
+
+// var fruits = [ "APPLE", "MANGO", "BANANA", "ORANGE" ]
+// for (var i = 0; i < fruits.length; i++) {
+//     fruits[i] = fruits[i].toLowerCase();
+// }
+// console.log(fruits);
+
+// Print each character of a given string using a loop.
+var str = "Hello World";
+for(var i=0;i<str.length;i++){
+    console.log(str[i])
+}
