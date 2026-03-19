56aa9ca71efff565d6c8eee920b5f6a8c2d1bd69
muzafarulhassan <muzafarulhassan86@gmail.com>
2026-03-19 13:01:00 -0700

    js

--- JS/assignment 3/evemt.js
+++ JS/assignment 3/evemt.js
@@ -67,3 +67,49 @@
 
 // var sentence="banana orange apple mango"
 // console.log(sentence.indexOf("banana"))
+
+
+
+// // Ask the user for a fruit name and tell how many times it appears in a paragraph
+// var paragraph="I like banana because  banana is healthy and banana is tasty."
+// var userFruit=prompt("Enter a fruit name:");
+// var count=0
+// var world=paragraph.split(" ")
+// for(var i=0;i<world.length;i++){
+//     if(world[i]===userFruit){
+//         count++;
+//     }
+// }
+// console.log("The fruit", userFruit, "appears", count,);    
+
+
+// // Create an array of 6 fruits and print only fruits that start with “b”.
+// var fruits=["apple", "mango", "banana", "orange","berry","blueberry"]
+// for(var i=0;i<fruits.length;i++){
+//     if(fruits[i][0]==="b"){
+//         console.log(fruits[i])
+//     }
+// }
+
+// // Take a sentence from the user and print the first 5 characters using slice.
+// var sentence="Banana"
+//  world=sentence.slice(0,2)
+// console.log(world)
+
+// // Take a sentence from the user and remove the last 3 characters using slice.
+// var sentence="banana is tasty"
+// world=sentence.slice(0,-3)
+// console.log(world)
+
+// // Ask the user for a word. If it ends with “fruit”. print “This ends with fruit”.
+// var userWord=prompt("Enter a word:");
+// if(userWord.endsWith("fruit")){
+//     console.log("This ends with fruit");
+// }else{
+//     console.log("This does not end with fruit");
+// }
+
+
+
+
+
